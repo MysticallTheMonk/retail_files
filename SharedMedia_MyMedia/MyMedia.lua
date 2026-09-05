2d95cc9c97e80ff28434339a91bd5422ce2ba1c2
MysticallTheMonk <monkmondaygames@gmail.com>
2026-09-05 14:44:52 -0400

    added shared media

---
+++ SharedMedia_MyMedia/MyMedia.lua
@@ -0,0 +1,24 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+LSM:Register("background", "d1", [[Interface\Addons\SharedMedia_MyMedia\background\d1.tga]]) 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "Expressway", [[Interface\Addons\SharedMedia_MyMedia\font\Expressway.ttf]]) 
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+LSM:Register("statusbar", "d1", [[Interface\Addons\SharedMedia_MyMedia\statusbar\d1.tga]]) 
